44844300a2ae9381e2b1e73b5a2fd4506277c952
kay-ws <a3.works@gmail.com>
2020-01-11 16:24:50 +0000

    SlackっぽいUIを作成

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,15 @@
 <template>
-  <div>
-    <nuxt />
-  </div>
+ <div class="app-layout">
+   <div class="sidebar">
+     <p>チャンネル一覧</p>
+     <p>#general</p>
+     <p>#random</p>
+     <p>#random</p>
+   </div>
+   <div class="main-content">
+     <nuxt />
+   </div>
+ </div>
 </template>
 
 <style>
@@ -22,6 +30,28 @@ html {
 *:after {
   box-sizing: border-box;
   margin: 0;
+}
+
+.app-layout {
+  display: flex;
+}
+
+.sidebar {
+  width: 300px;
+  background: #4a4141;
+  height: 100vh;
+  padding: 20px;
+}
+
+.sidebar p {
+  color: #dddddd;
+  padding-top: 4px;
+}
+
+.main-content {
+  width: 100%;
+  background: #f1f1f1;
+  height: 100vh;
 }
 
 .button--green {
