264d8609ae746e525bec41aca3ab7fc8aef6c88b
kay-ws <a3.works@gmail.com>
2020-01-12 04:37:36 +0000

    リアルタイムにチャットしようーチャットルームの移動

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,15 @@
 <template>
- <div class="app-layout">
-   <div class="sidebar">
-     <p>チャンネル一覧</p>
-     <p v-for="channel in channels">{{ channel.name }}</p>
-   </div>
-   <div class="main-content">
-     <nuxt />
-   </div>
- </div>
+  <div class="app-layout">
+    <div class="sidebar">
+      <p>チャンネル一覧</p>
+      <p v-for="channel in channels">
+        <nuxt-link :to="`/channels/${channel.id}`">{{ channel.name }}</nuxt-link>
+      </p>
+    </div>
+    <div class="main-content">
+      <nuxt />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -23,7 +25,7 @@ export default {
     db.collection('channels').get()
       .then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
-          this.channels.push(doc.data())
+          this.channels.push({id: doc.id, ...doc.data()})
         })
       })
     console.log(this.channels)
